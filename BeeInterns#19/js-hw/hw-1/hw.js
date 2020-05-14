603fe6bf6e05672e4542d1e705fd10cdd1635e3b
GrigSar <59669027+GrigSar@users.noreply.github.com>
2020-05-14 15:12:02 +0300

    Add files via upload

---
+++ BeeInterns#19/js-hw/hw-1/hw.js
@@ -0,0 +1,4 @@
+let name, admin;// Создать две переменные: name, admin
+name = "Grigor"; // В переменную name запишите своё имя
+admin = name;// В переменную admin запишите значение переменной name
+alert(admin);// выведите на экран admin, через функцию alert
